7e4265d2422707142d544a3e4c43aaf1eb0e7ef2
Ben Saada <bensaada12@gmail.com>
2026-03-10 18:17:45 +0200

    ☄️ work in progress Tue Mar 10 18:17:45 2026

--- terraform/iam-roles/main.tf
+++ terraform/iam-roles/main.tf
@@ -1,6 +1,6 @@
 module "iam-role" {
   for_each           = { for role in local.roles : role.role_name => role }
-  source             = "../terraform-modules/iam-role"
+  source             = "../../terraform-modules/iam-role"
   role_name          = each.key
   oidc_provider_bool = each.value.oidc_provider_bool
   oidc_provider      = each.value.oidc_provider
